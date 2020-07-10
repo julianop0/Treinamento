41b1df1dd7843a2f8f17b7b05b17d7b1ec39bc03
julianop0 <julianop.possamai@gmail.com>
2020-07-10 10:36:33 -0300

    10/07

--- www/App/Classes/Router.php
+++ www/App/Classes/Router.php
@@ -8,133 +8,147 @@ class Router
 {
 
     private $routes;
-    private $uri;
-    private $MethodWhoAcceptsParameters;
-    private $MethodWhoDoesntAcceptsParameters;
+    private $url;
 
+    private $object;
+    private $nameSpace;
 
-    function __construct()
-    {
-        $this->parseUrl();
-        $this->getRoutes();
-        $this->getMethodWhoAcceptsParameters();
-        $this->getWhoDoesntAcceptParameter();
-    }
+    private $controllerName;
+    private $method;
+    private $parameter;
 
     public function parseUrl()
     {
         if (isset($_GET['url'])) {
-            $this->uri = explode("/", rtrim($_GET['url']), FILTER_SANITIZE_URL);
+            return explode("/", rtrim($_GET['url']), FILTER_SANITIZE_URL);
         } else {
             header("location: listar");
         }
-        return $this->uri;
     }
 
     public function getRoutes(): array
     {
-        $this->routes = array(
+        return [
             "listar" => "ControllerList",
             "relatorio" => "ControllerPrint",
             "adicionar" => "ControllerAdd",
             "editar" => "ControllerUpdate",
             "deletar" => "ControllerDelete"
-        );
-
-        return $this->routes;
+        ];
     }
 
-    public function getWhoDoesntAcceptParameter(): array
+    public function getControllerName()
     {
-        $this->MethodWhoDoesntAcceptsParameters = array(
-            "ControllerAdd",
-            "CotnrollerPrint"
-        );
-
-        return $this->MethodWhoDoesntAcceptsParameters;
+        $this->url = $this->parseUrl();
+        $this->routes = $this->getRoutes();
+        return $this->routes[$this->url[0]];
     }
 
-    public function getMethodWhoAcceptsParameters(): array
+    public function validateRoute()
     {
-        $this->MethodWhoAcceptsParameters = array(
-            "ControllerUpdate/numeric/defaultMethod",
-            "ControllerUpdate/numeric/save",
-            "ControllerDelete/numeric/defaultMethod",
-            "ControllerList/string/ajaxCall"
-        );
+        $this->url = $this->parseUrl();
+        $this->routes = $this->getRoutes();
+        $this->controllerName = $this->getControllerName();
 
-        return $this->MethodWhoAcceptsParameters;
-    }
-
-    public function callController()
-    {
-
-        #Check valid route - error 404
-        if (array_key_exists($this->uri[0], $this->routes)) {
-
-            $controllerName = $this->routes[$this->uri[0]];
-
-            #Check route implementation - error 405
-            if (file_exists(DIR_CONTROLLER . $controllerName . ".php")) {
-
-                if (!in_array($controllerName, $this->MethodWhoDoesntAcceptsParameters)) {
-                    switch (count($this->uri)) {
-                        case 1:
-                            $this->uri["method"] = "defaultMethod";
-                            break;
-                        case 2:
-                            $this->uri["method"] = "defaultMethod";
-                            $this->uri["parameter"] = $this->uri[1];
-                            break;
-                        case 3:
-                            $this->uri["parameter"] = $this->uri[2];
-                            $this->uri["method"] = $this->uri[1];
-                            break;
-                    }
-                } else {
-                    switch (count($this->uri)) {
-                        case 1:
-                            $this->uri["method"] = "defaultMethod";
-                            break;
-                        case 2:
-                            $this->uri["method"] = $this->uri[1];
-                            break;
-                    }
-                }
-
-                $method = $this->uri["method"];
-
-                #Update/Delete parameter ID
-                foreach ($this->MethodWhoAcceptsParameters as $k => $v) {
-                    $value = explode("/", $v);
-                    if ($controllerName == $value[0] && $method == $value[2]) {
-                        if ($value[1] == "numeric") {
-                            #Preg - only numeric
-                            $parameter = (isset($this->uri["parameter"]) && preg_match('/^[0-9]{1,}/', $this->uri["parameter"])) ? $this->uri["parameter"] : null;
-                        } else {
-                            $parameter = filter_var($this->uri["parameter"], FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_LOW);
-                        }
-                    }
-                }
-
-                $nameSpace = "App\\Controller\\{$controllerName}";
-                $object = new $nameSpace;
-
-                if (empty($this->uri["parameter"])) {
-                    $object->$method();
-                } else {
-                    if ($method == "ajaxCall") {
-                        $arrParameters = explode("|", $parameter);
-                        $object->$method($arrParameters[0], $arrParameters[1]);
-                    } else {
-                        $object->$method($parameter);
-                    }
-                }
-            } else {
+        if (array_key_exists($this->url[0], $this->routes)) {
+            if (!file_exists(DIR_CONTROLLER . $this->controllerName . ".php")) {
                 echo "ERRO 405: Método não implementado";
+            } else {
+                return true;
             }
         } else {
             echo "ERRO 404: Esta página não existe";
+        }
+    }
+
+    public function getDynamicControllers()
+    {
+        return [
+            "ControllerUpdate" => "numeric",
+            "ControllerDelete" => "numeric",
+            "ControllerList" => "string"
+        ];
+    }
+
+    public function setMethodAndParameter()
+    {
+        $this->url = $this->parseUrl();
+
+        if (array_key_exists($this->getControllerName(), $this->getDynamicControllers())) {
+            switch (count($this->url)) {
+                case 1:
+                    $this->method = "defaultMethod";
+                    break;
+                case 2:
+                    $this->method = "defaultMethod";
+                    $this->parameter = $this->url[1];
+                    break;
+                case 3:
+                    $this->parameter = $this->url[2];
+                    $this->method = $this->url[1];
+                    break;
+            }
+        } else {
+            switch (count($this->url)) {
+                case 1:
+                    $this->method = "defaultMethod";
+                    break;
+                case 2:
+                    $this->method = $this->url[1];
+                    break;
+            }
+        }
+    }
+
+    public function getMethod()
+    {
+        return $this->method;
+    }
+
+    public function getParameter()
+    {
+        return $this->parameter;
+    }
+
+    public function validateParameter()
+    {
+        foreach ($this->getDynamicControllers() as $k => $v) {
+            $value = explode("/", $v);
+            if ($this->getControllerName() == $value[0]) {
+                if ($value[1] == "numeric") {
+                    #Preg - only numeric
+                    $this->parameter = (preg_match('/^[0-9]{1,}/', $this->parameter)) ? $this->uri["parameter"] : null;
+                } else {
+                    $this->parameter = filter_var($this->parameter, FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_LOW);
+                }
+            }
+        }
+    }
+
+    public function  callController()
+    {
+        if ($this->validateRoute()) {
+            $this->setMethodAndParameter();
+            if (array_key_exists($this->getControllerName(), $this->getDynamicControllers())) {
+                $this->validateParameter();
+            }
+
+            $this->nameSpace = "App\\Controller\\{$this->getControllerName()}";
+            $this->object = new $this->nameSpace;
+
+            $method = $this->getMethod();
+            $this->parameter = $this->getParameter();
+
+            if (empty($this->parameter)) {
+                $this->object->$method();
+            } else {
+                if ($this->method == "ajaxCall") {
+                    $arrParameters = explode("|", $this->parameter);
+                    $this->object->$method($arrParameters[0], $arrParameters[1]);
+                } else {
+                    $this->object->$method($this->parameter);
+                }
+            }
         }
     }
 }
--- www/App/views/print.php
+++ www/App/views/print.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="public/bootstrap4.5.0/css/bootstrap.css">
+  <link rel="stylesheet" href="public/bootstrap4.5.0/css/bootstrap.min.css">
   <style>
     th {
       font-size: 17px;
