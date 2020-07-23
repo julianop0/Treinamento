2c246671bc4e6a9bff6d526cd5a3d96ea9674eeb
julianop0 <julianop.possamai@gmail.com>
2020-07-23 10:24:41 -0300

    tests(model-veiculo): Atualização do progresso

--- www/tests/Models/VeiculoTest.php
+++ www/tests/Models/VeiculoTest.php
@@ -9,26 +9,64 @@ class VeiculoTest extends TestCase
 {
     public function setUp()
     {
-        $this->model = new Veiculo;
+        $this->model = $this->getMockBuilder(veiculo::class)
+            ->disableOriginalConstructor()
+            ->setMethods(['search', 'delete', 'add', 'update', 'selectAll'])
+            ->getMock();
     }
 
     public function test_if_search_returns_an_array()
     {
-        $this->assertIsArray($this->model->search());
+        $this->model->method('search')
+            ->with('alguma coisa')
+            ->willReturn(
+                [
+                    'veiculos' => [
+                        'dados do veiculo' => 'dados'
+                    ],
+                    'paginaAtual' => 1,
+                    'totalPaginas' => 2
+                ]
+            );
+
+        $this->assertIsArray($this->model->search('alguma coisa'));
+    }
+
+    public function test_if_selectAll_returns_an_array()
+    {
+        $this->model->method('selectAll')
+            ->willReturn(
+                [
+                    'id' => 1,
+                    'descricao' => 'veiculo aut'
+                ]
+            );
+
+        $this->assertIsArray($this->model->selectAll());
     }
 
     public function test_if_delete_returns_bool()
     {
-        $this->assertIsBool($this->model->delete());
+        $this->model->method('delete')
+            ->with('29')
+            ->willReturn(true);
+
+        $this->assertIsBool($this->model->delete('29'));
     }
 
     public function test_if_add_returns_bool()
     {
+        $this->model->method('add')
+            ->willReturn(true);
+
         $this->assertIsBool($this->model->add());
     }
 
     public function test_if_update_returns_bool()
     {
+        $this->model->method('update')
+            ->willReturn(true);
+
         $this->assertIsBool($this->model->update());
     }
 }
